14f100ebcc390e35bda9400f97f40d6d671d09b8
a304215 <andy940047@gmail.com>
2020-02-13 01:34:51 +0800

    new code

---
+++ force-app/main/default/lwc/andy_v1/andy_v1.js
@@ -0,0 +1,77 @@
+import { LightningElement, api, track ,wire} from 'lwc';
+import ReturnPBEList from '@salesforce/apex/NewProductByDiscount.ReturnPBEList';
+//import ReturnPBEList_search from '@salesforce/apex/NewProductByDiscount.ReturnPBEList_search';
+const columns = [ 
+    { label: 'Name', fieldName: 'nameUrl',
+    type: 'url',
+    typeAttributes: {label:{fieldName:'Name'},target: '_blank'}}, 
+    { label: 'part number', fieldName: 'productcode'},
+    { label: 'List Price', fieldName: 'nuitprice'}
+];
+export default class DemoButtonMenu extends LightningElement {
+    @api recordId;
+    @track product;
+    @track error;
+    @track add_product_choose_product = false;
+    @track columns = columns;
+    @track test_list = [1];
+    @track myValue;
+    @track queryTerm;
+    @track
+    items = [
+        {
+            id: 'menu-item-1',
+            label: 'add product',
+            value: 'add_product',
+        },
+        {
+            id: 'menu-item-2',
+            label: 'edit product',
+            value: 'edit_product',
+        },
+        {
+            id: 'menu-item-3',
+            label: 'Gamma',
+            value: 'gamma',
+        }
+    ];
+    @wire(ReturnPBEList,{opportunity_id:'$recordId'})
+    wiredContacts({ error, data }) {
+        if (data) {
+            this.product = data;
+            this.do_for();
+        } else if (error) {
+            this.error = error;
+            this.contacts = undefined;
+        }
+    }
+    do_for(){
+        for(let i =0;i<this.product.length;i++){
+            this.test_list[i] = {
+                Id : this.product[i].Id,
+                Name :this.product[i].Name,
+                productcode:this.product[i].Product2.ProductCode,
+                nuitprice:this.product[i].UnitPrice.toFixed(2).toString(),
+                nameUrl:'/lightning/r/PricebookEntry/'+this.product[i].Id+'/view'
+            };
+        }
+    }
+    handleMenuSelect(event) {//this function is for add_product
+        const chooses = event.detail.value;
+        if(chooses === "add_product"){
+            this.add_product_choose_product= true;
+        }
+    }
+    close_page(){//this function is for add_product
+        this.add_product_choose_product = false;
+    }
+    next_page(){//this function is for add_product
+        
+    }
+    handleKeyUp(evt) {
+        const isEnterKey = evt.keyCode === 13;
+        if (isEnterKey) {
+            this.queryTerm = evt.target.value;
+        }
+    }
+}
